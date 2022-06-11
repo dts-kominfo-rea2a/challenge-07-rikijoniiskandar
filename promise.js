f799fb17ea2194ca6212453ac1a7c7045fe0f3d3
Riki Joni Iskandar <rikijoniiskandar97@gmail.com>
2022-06-11 14:00:29 +0700

    created function promiseInput

--- promise.js
+++ promise.js
@@ -1,8 +1,14 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
-
+const promiseOutput = async (emosi) => {
+  let dataTheaterIXX = await promiseTheaterIXX();
+  let dataTheaterVGC = await promiseTheaterVGC();
+  let resultTheater = dataTheaterIXX.concat(dataTheaterVGC);
+  let totalEmosi = 0;
+  resultTheater.map(x => x.hasil === emosi && (totalEmosi += 1) )
+  return totalEmosi
+};
 module.exports = {
   promiseOutput,
 };
